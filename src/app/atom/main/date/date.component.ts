2695460079f966aa746322edfadd06027c5eb75a
YaroslavHolovatyi <yaroslavgolovatyy@gmail.com>
2023-06-23 00:47:42 +0300

    small Changes

--- src/app/atom/main/date/date.component.ts
+++ src/app/atom/main/date/date.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-date',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./date.component.scss']
 })
 export class DateComponent {
+  @Input() smaller: boolean = false;
 
   date: Date = new Date();
 
